58ded7eb54ca390da9e64f982fc83a2ae7a45770
Charlie Revett <charlierevett@gmail.com>
2014-11-04 17:37:23 +0000

    Refactor Crimp module

--- lib/crimp.rb
+++ lib/crimp.rb
@@ -2,29 +2,46 @@ require "crimp/version"
 require "digest"
 
 module Crimp
-  def self.stringify(obj)
-    if obj.class == Hash
-      arr = []
-      obj.each do |key, value|
-        arr << "#{self.stringify key}=>#{self.stringify value}"
-      end
-      obj = arr
-    end
-    if obj.class == Array
-      str = ''
-      obj.map! do |value|
-        self.stringify value
-      end.sort!.each do |value|
-        str << value
-      end
-    end
-    if obj.class != String
-      obj = obj.to_s << obj.class.to_s
-    end
-    obj
+  def self.signature(obj)
+    Digest::MD5.hexdigest stringify(obj)
   end
 
-  def self.signature(obj)
-    Digest::MD5.hexdigest( self.stringify obj )
+  def self.stringify(obj)
+    case obj
+    when Array
+      convert_array obj
+    when Hash
+      convert_hash obj
+    when String
+      obj
+    else
+      convert_other obj
+    end
+  end
+
+  private
+
+  def self.convert_array(array)
+    ''.tap do |string|
+      stringify_elements(array).sort!.each { |e| string << e }
+    end
+  end
+
+  def self.convert_hash(hash)
+    stringify hash_to_array(hash)
+  end
+
+  def self.convert_other(obj)
+    "#{obj.to_s}#{obj.class.to_s}"
+  end
+
+  def self.hash_to_array(hash)
+    [].tap do |a|
+      hash.each { |k, v| a << "#{stringify k}=>#{stringify v}" }
+    end
+  end
+
+  def self.stringify_elements(array)
+    array.map! { |e| stringify(e) }
   end
 end

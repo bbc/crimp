03afa52787dc9669ddc79373f89ab2fbd4ea4968
Charlie Revett <charlierevett@gmail.com>
2014-11-05 15:45:32 +0000

    Stringify arrays after being parsed
    - Picked up using the migration tests added in previous commit.
    - Also did some more refactoring.

--- lib/crimp.rb
+++ lib/crimp.rb
@@ -7,41 +7,43 @@ module Crimp
   end
 
   def self.stringify(obj)
-    case obj
-    when Array
-      convert_array obj
-    when Hash
-      convert_hash obj
-    when String
-      obj
-    else
-      convert_other obj
-    end
+    convert(obj).tap { |o| return o.class == String ? o : to_string(o) }
   end
 
   private
 
-  def self.convert_array(array)
-    ''.tap do |string|
-      stringify_elements(array).sort!.each { |e| string << e }
+  def self.convert(obj)
+    case obj
+    when Array
+      parse_array obj
+    when Hash
+      parse_hash obj
+    when String
+      obj
+    else
+      to_string obj
     end
-  end
-
-  def self.convert_hash(hash)
-    stringify hash_to_array(hash)
-  end
-
-  def self.convert_other(obj)
-    "#{obj.to_s}#{obj.class.to_s}"
   end
 
   def self.hash_to_array(hash)
     [].tap do |a|
-      hash.each { |k, v| a << "#{stringify k}=>#{stringify v}" }
+      hash.each { |k, v| a << pair_to_string(k, v) }
     end
   end
 
-  def self.stringify_elements(array)
-    array.map! { |e| stringify(e) }
+  def self.pair_to_string(k, v)
+    "#{stringify k}=>#{stringify v}"
+  end
+
+  def self.parse_array(array)
+    array.map! { |e| stringify(e) }.sort!
+  end
+
+  def self.parse_hash(hash)
+    stringify hash_to_array(hash)
+  end
+
+  def self.to_string(obj)
+    "#{obj.to_s}#{obj.class.to_s}"
   end
 end

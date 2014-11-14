154f63e0c9b110a958affd1da9c8f00f5ce7aef7
Charlie Revett <charlierevett@gmail.com>
2014-11-14 14:22:58 +0000

    Refactor code & improve test coverage
    Closes #1

--- Rakefile
+++ Rakefile
@@ -1,9 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
-require 'rspec/core/rake_task'
 require 'bundler/gem_tasks'
-require 'crimp'
-
-RSpec::Core::RakeTask.new(:spec)
+require 'rake/rspec'
 
 task :default => :spec
--- lib/crimp.rb
+++ lib/crimp.rb
@@ -2,29 +2,48 @@ require "crimp/version"
 require "digest"
 
 module Crimp
-  def self.stringify(obj)
-    if obj.class == Hash
-      arr = []
-      obj.each do |key, value|
-        arr << "#{self.stringify key}=>#{self.stringify value}"
-      end
-      obj = arr
-    end
-    if obj.class == Array
-      str = ''
-      obj.map! do |value|
-        self.stringify value
-      end.sort!.each do |value|
-        str << value
-      end
-    end
-    if obj.class != String
-      obj = obj.to_s << obj.class.to_s
-    end
-    obj
+  def self.signature(obj)
+    Digest::MD5.hexdigest stringify(obj)
   end
 
-  def self.signature(obj)
-    Digest::MD5.hexdigest( self.stringify obj )
+  def self.stringify(obj)
+    convert(obj).tap { |o| return o.class == String ? o : to_string(o) }
+  end
+
+  private
+
+  def self.convert(obj)
+    case obj
+    when Array
+      parse_array obj
+    when Hash
+      parse_hash obj
+    when String
+      obj
+    else
+      to_string obj
+    end
+  end
+
+  def self.hash_to_array(hash)
+    [].tap do |a|
+      hash.each { |k, v| a << pair_to_string(k, v) }
+    end
+  end
+
+  def self.pair_to_string(k, v)
+    "#{stringify k}=>#{stringify v}"
+  end
+
+  def self.parse_array(array)
+    array.map! { |e| stringify(e) }.sort!
+  end
+
+  def self.parse_hash(hash)
+    stringify hash_to_array(hash)
+  end
+
+  def self.to_string(obj)
+    "#{obj.to_s}#{obj.class.to_s}"
   end
 end

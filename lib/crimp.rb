cbe656769500bfe59e09f22cf02a3e2b07e85163
Ettore Berardi <ettore.berardi@bbc.co.uk>
2018-08-08 13:59:57 +0100

    Fix signature mod between Ruby 2.1 and 2.4.
    For non Sting scalar values the object class gets injected in the
    signature data.
    
    Ruby 2.4 unified Fixnum and Bignum into Integer so a signature
    contening numbers in 2.1 will be different from one generated in 2.1.
    
    If you'll use Crimp to generate cache/database ids this can be a
    problem.
    
    This commit makes Crimp backward compatible.

--- lib/crimp.rb
+++ lib/crimp.rb
@@ -20,6 +20,8 @@ module Crimp
       parse_hash obj
     when String
       obj
+    when Integer
+      integer_to_string obj
     else
       to_string obj
     end
@@ -45,5 +47,20 @@ module Crimp
 
   def self.to_string(obj)
     "#{obj}#{obj.class}"
+  end
+
+  # This is for legacy/compatibilty reason:
+  #
+  # Ruby 2.1
+  # 2.class => Fixnum
+  # Ruby > 2.4
+  # 2.class => Integer
+  #
+  # Say you have a huge number of stored keys and you migrate your app from 2.1 to >= 2.4
+  # this would cause a change of the signature for a subset of the keys which would be hard
+  # to debug especially for nested data structures.
+  #
+  def self.integer_to_string(obj)
+    "#{obj}Fixnum"
   end
 end
